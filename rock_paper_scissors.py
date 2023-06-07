33e5f9ee34672c21f7f8c6363d601fec7e42d7c2
izdravko397 <133270937+izdravko397@users.noreply.github.com>
2023-06-07 18:09:19 +0300

    I maked my code structured whit functions

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -8,9 +8,7 @@ wins = 0
 loses = 0
 draws = 0
 
-while True:
-    player_move = input("Choose [r]ock, [p]aper or [s]cissors: ")
-
+def player_turn(player_move):
     if player_move == "r":
         player_move = rock
     elif player_move == "p":
@@ -19,8 +17,11 @@ while True:
         player_move = scissors
     else:
         print("Invalis Input. Try again...")
-        continue
+        return True
 
+    return player_move
+
+def computer_move():
     computer_random_number = random.randint(1, 3)
 
     computer_move = ""
@@ -33,33 +34,59 @@ while True:
         computer_move = scissors
 
     print(f"The computer chose {computer_move}.")
+    return computer_move
 
-    if (player_move == rock and computer_move == scissors)\
-        or (player_move == paper and computer_move == rock)\
-        or (player_move == scissors and computer_move == paper):
+def analizating_resule():
+    wins = 0
+    loses = 0
+    draws = 0
+
+    player_move = player_turn(input("Choose [r]ock, [p]aper or [s]cissors: "))
+
+    if player_move == True:
+        return wins, loses, draws, True
+
+    computer = computer_move()
+
+    if (player_move == rock and computer == scissors)\
+        or (player_move == paper and computer == rock)\
+        or (player_move == scissors and computer == paper):
         print("You win!")
         wins += 1
-    elif player_move == computer_move:
+    elif player_move == computer:
         print("Draw!")
         draws += 1
     else:
         print("You lose!")
         loses += 1
 
-    print(f"Results: Wins: {wins}; Loses: {loses}; Draws: {draws}")
+    return wins, loses, draws, False
 
-    break_game_flag = False
+def play_again_checking(play_again):
     while True:
-        play_again = input("Type [yes] to Play Again or [no] to quit: ")
-
         if play_again.lower() == "yes":
-            break
+            return True
         elif play_again.lower() == "no":
-            break_game_flag = True
-            break
+            raise SystemExit("Thank you for playing!")
         else:
             print("Invalid input. Please try again...")
+            play_again = input("Type [yes] to Play Again or [no] to quit: ")
             continue
 
-    if break_game_flag:
-        raise SystemExit("Thank you for playing!")
+while True:
+
+    wins_values, loses_values, draws_values, flag_result = analizating_resule()
+
+    if flag_result:
+        continue
+
+    wins += wins_values
+    loses += loses_values
+    draws += draws_values
+
+    print(f"Results: Wins: {wins}; Loses: {loses}; Draws: {draws}")
+
+    play_again = play_again_checking(input("Type [yes] to Play Again or [no] to quit: "))
+
+    if play_again:
+        continue
